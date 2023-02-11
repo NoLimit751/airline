467ecf7037f7919f7625465ff2b4ecc3c47e115a
NoLimit751 <41649970+NoLimit751@users.noreply.github.com>
2023-02-11 16:47:16 +0100

    Reducing power of oil inventory policy
    Changed how much each polity shields from price fluctuation to make game harder in this aspect.

--- airline-data/src/main/scala/com/patson/model/oil/OilInventoryPolicy.scala
+++ airline-data/src/main/scala/com/patson/model/oil/OilInventoryPolicy.scala
@@ -15,9 +15,9 @@ case class OilInventoryPolicy(airline : Airline, factor : Double, startCycle : I
   }
   
   val option = factor match {
-    case 0.9 => CONSERVATIVE
-    case 0.5 => BALANCED
-    case 0.2 => AGGRESSIVE
+    case 0.5 => CONSERVATIVE
+    case 0.3 => BALANCED
+    case 0.1 => AGGRESSIVE
     case 0 => NONE
   }
 }
@@ -28,9 +28,9 @@ object OilInventoryPolicy {
   def byOption(option : OilInventoryPolicyOption.Value, airline : Airline, startCycle : Int) = {
     val factor : Double = 
       option match {
-        case CONSERVATIVE => 0.9
-        case BALANCED => 0.5
-        case AGGRESSIVE => 0.2
+        case CONSERVATIVE => 0.5
+        case BALANCED => 0.3
+        case AGGRESSIVE => 0.1
         case NONE => 0
       }
     
@@ -38,14 +38,14 @@ object OilInventoryPolicy {
   }
   
   def getDefaultPolicy(airline : Airline) = {
-    byOption(CONSERVATIVE, airline, 0) 
+    byOption(NONE, airline, 0) 
   }
   
   val description = (value : Value) => {
     value match {
-        case CONSERVATIVE => "Conservative - shields from 90% of price fluctuation"
-        case BALANCED => "Balanced - shields from 50% of price fluctuation"
-        case AGGRESSIVE => "Aggressive - shields from 20% of price fluctuation"
+        case CONSERVATIVE => "Conservative - shields from 50% of price fluctuation"
+        case BALANCED => "Balanced - shields from 30% of price fluctuation"
+        case AGGRESSIVE => "Aggressive - shields from 10% of price fluctuation"
         case NONE => "No Inventory - buys all required fuel at market price"
       }
   }
